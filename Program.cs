2ddefcf0c68cfc3592d1665079fe79efe0317429
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-03 09:19:47 -0700

    deserializes JSON response into article class properties

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using RestSharp;
 
@@ -13,7 +14,16 @@ namespace ApiTest
             var apiCallTask = ApiHelper.ApiCall("a8N9RYvKvHtG8rQCQlv2jCJCqNvEbptD");
             var result = apiCallTask.Result;
             JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
-            Console.WriteLine(jsonResponse["results"]);
+            List<Article> articleList = JsonConvert.DeserializeObject<List<Article>>(jsonResponse["results"].ToString());
+
+            foreach (Article article in articleList)
+            {
+                Console.WriteLine($"Section: {article.Section}");
+                Console.WriteLine($"Title: {article.Title}");
+                Console.WriteLine($"Abstract: {article.Abstract}");
+                Console.WriteLine($"Url: {article.Url}");
+                Console.WriteLine($"Byline: {article.Byline}");
+            }
         }
     }
 
@@ -26,5 +36,17 @@ namespace ApiTest
             var response = await client.ExecuteTaskAsync(request);
             return response.Content;
         }
+    }
+
+    // you could just as easily call the class below by the name "JsonResponse"
+    // in practice, we will review the JSON response to build this class
+    // each property should match the name and tier (one level? two?) found therein
+    public class Article
+    {
+        public string Section { get; set; }
+        public string Title { get; set; }
+        public string Abstract { get; set; }
+        public string Url { get; set; }
+        public string Byline { get; set; }
     }
 }
