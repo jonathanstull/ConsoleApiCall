627d39d8072176676d6807266545e17ba923fb03
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-03 08:28:49 -0700

    initial commit with RestSharp call to NYT top stories

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace ApiTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            var apiCallTask = ApiHelper.ApiCall("a8N9RYvKvHtG8rQCQlv2jCJCqNvEbptD");
+            var result = apiCallTask.Result;
+            Console.WriteLine(result);
+        }
+    }
+
+    class ApiHelper
+    {
+        public static async Task<string> ApiCall(string apiKey)
+        {
+            RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+            RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.GET);
+            var response = await client.ExecuteTaskAsync(request);
+            return response.Content;
+        }
+    }
+}
